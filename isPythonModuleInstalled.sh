4fd4a453c68ba556938dead1f5a380d5fbcf1c4e
VRTechnologiesForAutomation <137881855+VRTechnologiesForAutomation@users.noreply.github.com>
2024-01-20 12:37:08 +0530

    Add files via upload

---
+++ isPythonModuleInstalled.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+moduleName=${1}
+python3 -c "import ${moduleName}" 1>/dev/null 2>/dev/nll
+if [[ $? -eq 0 ]] ; then
+  echo "The ${moduleName} module is installed"
+else
+  echo "The ${moduleName} module is not installed"
+fi
